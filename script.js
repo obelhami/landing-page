9ab92546871a7b8b5b3e93fc9020c854f9757a55
obelhami <obelhami@e2r2p10.1337.ma>
2025-03-20 17:29:01 +0000

    pushing

---
+++ script.js
@@ -0,0 +1,48 @@
+// const sections = [
+//     {
+//         title: 'Grow Together',
+//         description: `Generate meaningful discussions with your audience and build a strong, loyal community. Think of the insightful conversations you miss out on with a feedback form.`,
+//         image: './images/illustration-mockups.svg',
+//         id: 0,
+//     },
+//     {
+//         title: 'Flowing Conversations',
+//         description: `You wouldn't paginate a conversation in real life, so why do it online? Our threads have just-in-time loading for a more natural flow.`,
+//         image: './images/illustration-grow-together.svg',
+//         id: 1,
+//     },
+//     {
+//         title: 'Your Users',
+//         description: `It takes no time at all to integrate Huddlle with your app's authentication solution. This means, once signed in to your app, your users can start chatting immediately.`,
+//         image: './images/illustration-your-users.svg',
+//         id: 2,
+//     },
+//     {
+//         title: ' omar belhamid',
+//         description: `It takes no time at all to integrate Huddlle with your app's authentication solution. This means, once signed in to your app, your users can start chatting immediately.`,
+//         image: './images/illustration-your-users.svg',
+//         id: 3,
+//     },
+//     {
+//         title: ' Zahira',
+//         description: `It takes no time at all to integrate Huddlle with your app's authentication solution. This means, once signed in to your app, your users can start chatting immediately.`,
+//         image: './images/illustration-grow-together.svg',
+//         id: 4,
+//     },
+// ]
+// const secondSection = document.getElementById('second_section')
+
+// sections.map((e) => {
+//     const element = document.createElement('div')
+//     element.classList = 'grow'
+//     element.innerHTML = `
+//             <div class="your_users">
+//                 <h2>${e.title}</h2>
+//                 <p>${e.description}</p>
+//             </div>
+//             <img src="${e.image}" alt=${e.title} class="together">`
+//     secondSection.appendChild(element);
+// })
+
+
+
